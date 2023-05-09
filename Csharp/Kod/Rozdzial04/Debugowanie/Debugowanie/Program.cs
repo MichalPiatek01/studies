32d5eac4d8f9b3eb446c3b02af11cf7d5096d1ba
MichalPiatek01 <117731230+MichalPiatek01@users.noreply.github.com>
2023-05-09 19:02:54 +0200

    Add files via upload

---
+++ Csharp/Kod/Rozdzial04/Debugowanie/Debugowanie/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace Debugging
+{
+    class Program
+    {
+        static double Dodaj(double a, double b)
+        {
+            return a + b; // celowo wprowadzony błąd
+        }
+
+        static void Main(string[] args)
+        {
+            double a = 4.5; // możesz też użyć słowa kluczowego var
+            double b = 2.5;
+            double odpowiedz = Dodaj(a, b);
+            WriteLine($"{a} + {b} = {odpowiedz}");
+            ReadLine(); // czekaj, aż użytkownik naciśnie ENTER
+        }
+    }
+}
---
+++ Csharp/Kod/Rozdzial04/KalkulatorBiblioteka/KalkulatorBiblioteka/Kalkulator.cs
@@ -0,0 +1,10 @@
+﻿namespace CS7
+{
+    public class Kalkulator
+    {
+        public double Dodaj(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
