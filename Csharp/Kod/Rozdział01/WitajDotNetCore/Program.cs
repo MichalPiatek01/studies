f5b19bf2849a3ac7e5ec23334655288e3d0f9c1a
MichalPiatek01 <117731230+MichalPiatek01@users.noreply.github.com>
2023-05-09 18:50:13 +0200

    Add files via upload

---
+++ Csharp/Kod/Rozdział01/WitajDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WitajDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            System.Console.WriteLine("Witaj, .NET Core!");
+        }
+    }
+}
---
+++ Csharp/Kod/Rozdział01/WitajDotNetCore/program/program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Reflection;
+
+using System;
+
+namespace WitajDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Data.DataSet ds;
+            System.Net.Http.HttpClient client;
+            // przejrzyj w pętli zestawy, do których odwołuje się aplikacja
+            foreach (var r in Assembly.GetEntryAssembly()
+            .GetReferencedAssemblies())
+            {
+                // załaduj zestaw, żeby można było odczytać szczegóły na jego temat
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+                // zadeklaruj zmienną zliczającą ogólną liczbę metod;
+                int methodCount = 0;
+                // przejrzyj w pętli wszystkie typy z zestawu
+                foreach (var t in a.DefinedTypes)
+                {
+                    // zsumuj liczbę metod
+                    methodCount += t.GetMethods().Count();
+                }
+                // wypisz liczbę typów i ich metod
+                Console.WriteLine($"W zestawie {r.Name} jest {a.DefinedTypes.Count():N0} typów " +
+                    $"i {methodCount:N0} metod");
+            }
+        }
+    }
+}
