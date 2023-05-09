2a9ec8dcc1a3703ea35775aa15356e0e9f5e0181
MichalPiatek01 <117731230+MichalPiatek01@users.noreply.github.com>
2023-05-09 18:51:53 +0200

    Add files via upload

---
+++ Csharp/Kod/Rozdzial02/Argumenty/Argumenty/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+using System;
+
+namespace Argumenty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), args[0], true);
+            BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), args[1], true);
+
+            try
+            {
+                WindowWidth = int.Parse(args[2]);
+                WindowHeight = int.Parse(args[3]);
+            }
+            
+            catch(PlatformNotSupportedException)
+            {
+                WriteLine("Aktualna platforma nie pozwala na zmianę wielkości okna konsoli.");
+            }
+            
+            WriteLine($"Otrzymano {args.Length} argumentów");
+            foreach ( string arg in args ) 
+            { 
+            WriteLine( arg );
+            }
+
+        }
+    }
+}
---
+++ Csharp/Kod/Rozdzial02/Działanie_na_zmiennych/Działanie_na_zmiennych/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+namespace Działania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Większość operatorów jest dwuargumentowa
+            //var wynikOperacji = pierwszyArgument operator drugiArgument;
+            // Część operatorów jest jednoargumentowa
+            //var wynikOperacji = pierwszyArgument operator;
+            // Istnieją też operatory trójargumentowe
+            //var wynikOperacji = pierwszyArgument pierwszyOperator drugiArgument drugiOperator trzeciArgument;
+
+            int i = 3;
+            Console.WriteLine(i);
+            //int y = i++;
+            int y = ++i;
+            Console.WriteLine(y);
+            Console.WriteLine(i);
+
+        }
+    }
+}
