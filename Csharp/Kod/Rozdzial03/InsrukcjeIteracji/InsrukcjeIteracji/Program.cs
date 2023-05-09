63ee74e7df6621db27b9bf5b0878397a4f6d0619
MichalPiatek01 <117731230+MichalPiatek01@users.noreply.github.com>
2023-05-09 19:02:02 +0200

    Add files via upload

---
+++ Csharp/Kod/Rozdzial03/InsrukcjeIteracji/InsrukcjeIteracji/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+class IterationInstructions
+{
+    static void Main(string[] args)
+    {
+        int x = 0;
+        while (x < 10)
+        {
+            Console.WriteLine(x);
+            x++ ;
+        }
+
+        string haslo = string.Empty;
+        do
+        {
+            WriteLine("Podaj swoje haslo: ");
+            haslo = ReadLine();
+        } while (haslo != "sekret");
+        WriteLine("Tak jest");
+
+        for (int y = 1; y <= 10; y++)
+        {
+            WriteLine(y);
+        }
+
+        string[] imiona = { "Maria", "Bartek", "Dariusz" };
+        foreach (string imie in imiona)
+        {
+            WriteLine($"{imie} ma {imie.Length} znaków");
+        }
+    }
+}
---
+++ Csharp/Kod/Rozdzial03/WykrywaniePrzepelnien/WykrywaniePrzepelnien/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+
+namespace MyApp
+{
+    class WykrywaniePrzepelnien
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                checked
+                {
+                    int x = int.MaxValue - 1;
+                    WriteLine(x);
+                    x++;
+                    WriteLine(x);
+                    x++;
+                    WriteLine(x);
+                    x++;
+                    WriteLine(x);
+                    x++;
+                    WriteLine(x);
+                }
+            }
+            catch (OverflowException)
+            {
+                WriteLine("Wystąpiło przepełnienie ale złapałem wyjątek");
+            }
+            
+            unchecked
+            {
+                int y = int.MaxValue + 1;
+                WriteLine(y); // wypisze -2147483648
+                y--;
+                WriteLine(y); // wypisze 2147483647
+                y--;
+                WriteLine(y); // wypisze 2147483646
+            }
+
+            int max = 500;
+            for (byte i = 0; i < max; i++)
+            {
+                WriteLine(i);
+            }
+        }
+    }
+}
