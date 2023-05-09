3d021fcb773689f89163ef54741490047c38c886
MichalPiatek01 <117731230+MichalPiatek01@users.noreply.github.com>
2023-05-09 19:05:58 +0200

    Add files via upload

---
+++ Csharp/Kod/Rozdzial04/KalkulatorBibliotekaTesty/KalkulatorTesty.cs
@@ -0,0 +1,34 @@
+using CS7;
+using Xunit;
+namespace KalkulatorBibliotekaTesty
+{
+    public class KalkulatorTesty
+    {
+        [Fact]
+        public void TestDodaj2i2()
+        {
+            // przygotuj
+            double a = 2;
+            double b = 2;
+            double oczekiwane = 4;
+            var kal = new Kalkulator();
+            // wykonaj
+            double wynik = kal.Dodaj(a, b);
+            // sprawdü
+            Assert.Equal(oczekiwane, wynik);
+        }
+        [Fact]
+        public void TestDodaj2i3()
+        {
+            // przygotuj
+            double a = 2;
+            double b = 3;
+            double oczekiwane = 5;
+            var kal = new Kalkulator();
+            // wykonaj
+            double wynik = kal.Dodaj(a, b);
+            // sprawdü
+            Assert.Equal(oczekiwane, wynik);
+        }
+    }
+}
