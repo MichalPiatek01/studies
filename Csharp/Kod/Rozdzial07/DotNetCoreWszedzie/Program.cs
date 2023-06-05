68d57fd50f50d8920be803b2c45dec61baf0d8af
MichalPiatek01 <117731230+MichalPiatek01@users.noreply.github.com>
2023-06-05 20:34:00 +0200

    Add files via upload

---
+++ Csharp/Kod/Rozdzial07/DotNetCoreWszedzie/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+namespace DotNetCoreWszedzie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Mogę działać wszędzie!");
+        }
+    }
+}
