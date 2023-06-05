4b93627f5f40314a62865d217739b9e9e7b88255
MichalPiatek01 <117731230+MichalPiatek01@users.noreply.github.com>
2023-06-05 20:32:46 +0200

    Add files via upload

---
+++ Csharp/Kod/Rozdzial07/BibliotekaWspolna/BibliotekaWspolna/RozszerzeniaDlaString.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+namespace CS7
+{
+    public static class RozszerzeniaDlaString
+    {
+        public static bool PoprawnyZnacznikXML(this string wejscie)
+        {
+            return Regex.IsMatch(wejscie, @"^<([a-z]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)$");
+        }
+        public static bool PoprawneHaslo(this string wejscie)
+        {
+            // przynajmniej osiem poprawnych znaków
+            return Regex.IsMatch(wejscie, "^[a-zA-Z0-9_-]{8,}$");
+        }
+        public static bool LiczbaSzesnastkowa(this string wejscie)
+        {
+            // trzy lub sześć poprawnych cyfr szesnastkowych
+            return Regex.IsMatch(wejscie, "^#?([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$");
+        }
+    }
+}
