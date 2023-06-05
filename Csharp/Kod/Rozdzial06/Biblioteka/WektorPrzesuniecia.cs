bee0d1aed72d1da947f411e4536a3976c8d0eec7
MichalPiatek01 <117731230+MichalPiatek01@users.noreply.github.com>
2023-06-05 20:27:54 +0200

    Add files via upload

---
+++ Csharp/Kod/Rozdzial06/Biblioteka/WektorPrzesuniecia.cs
@@ -0,0 +1,19 @@
+﻿namespace CS7
+{
+    public struct WektorPrzesuniencia
+    {
+        public int X;
+        public int Y;
+
+        public WektorPrzesuniencia(int poczatkoweX, int poczatkoweY)
+        {
+            X = poczatkoweX;
+            Y = poczatkoweY;
+        }
+
+        public static WektorPrzesuniencia operator +(WektorPrzesuniencia wektor1, WektorPrzesuniencia wektor2)
+        {
+            return new WektorPrzesuniencia(wektor1.X + wektor2.X, wektor1.Y + wektor2.Y);
+        }
+    }
+}
