535420550c97fea2695af03b67deab5cfa3fa559
MichalPiatek01 <117731230+MichalPiatek01@users.noreply.github.com>
2023-06-05 20:24:26 +0200

    Add files via upload

---
+++ Csharp/Kod/Rozdzial06/Biblioteka/OsobaComparer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+namespace CS7
+{
+    public class OsobaComparer : IComparer<Osoba>
+    {
+        public int Compare(Osoba x, Osoba y)
+        {
+            // porównaj długości nazwisk...
+            int temp = x.Nazwisko.Length.CompareTo(y.Nazwisko.Length);
+            /// …a jeżeli są równe…
+            if (temp == 0)
+            {
+                // …to posortuj alfabetycznie…
+                return x.Nazwisko.CompareTo(y.Nazwisko);
+            }
+            else
+            {
+                // …poza tym sortuj według długości
+                return temp;
+            }
+        }
+    }
+}
